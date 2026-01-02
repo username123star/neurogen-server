ec5410d4ac38a88ef5775ad23cbedac84e72ee5c
username123star <ismailolasunkanmibasit@gmail.com>
2026-01-02 09:34:16 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const app = require('./app');
+const { PORT } = require('./config/env');
+const logger = require('./utils/logger');
 
+app.listen(PORT, () => {
+  logger.info(`NeuroGen backend running on port ${PORT}`);
+});
